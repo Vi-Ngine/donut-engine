1ba5544ac4e93bbac5874bc2189775c7164a8a0f
gameoflord012 <manhhieu012@gmail.com>
2023-07-07 17:42:07 +0700

    project migrate

---
+++ desktop/src/main/java/donut/desktop/DonutEditorDesktopLauncher.java
@@ -0,0 +1,20 @@
+package donut.desktop;
+
+import donut.core.editor.service.DonutConsole;
+import donut.core.editor.service.DonutServiceManager;
+
+public class DonutEditorDesktopLauncher {
+    public static void main(String[] args)
+    {
+        DonutServiceManager serviceManager = new DonutServiceManager();
+
+        serviceManager.addService(new DonutConsole());
+        serviceManager.startServices();
+
+        try {
+            serviceManager.joins();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
